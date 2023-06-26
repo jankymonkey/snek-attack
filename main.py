44dc1e4264ee2f88fa63eaccdcd60245b11bd6b6
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2023-06-26 15:28:25 +0800

    Locate Submit button

--- main.py
+++ main.py
@@ -26,17 +26,24 @@ while True:
         print(play_button)
         play_button_center = pyautogui.center(play_button)
         play_button_x, play_button_y = play_button_center
+        print("run game button")
+        print(play_button_x, play_button_y)
         pyautogui.click(play_button_x / 2, play_button_y / 2)
         break
 
-# while True:
-#     submit = pyautogui.locateOnScreen("./submit.png", grayscale=True, confidence=0.5)
-#     time.sleep(1)
-#     if submit != None:
-#         submit_center = pyautogui.center(submit)
-#         submit_x, submit_y = submit_center
-#         pyautogui.click(submit_x / 2, submit_y / 2)
-#         break
+while True:
+    submit = pyautogui.locateOnScreen("./submit.png", confidence=0.8)
+
+    time.sleep(1)
+
+    if submit != None:
+        submit_center = pyautogui.center(submit)
+        submit_x, submit_y = submit_center
+        print("submit button")
+        print(submit_x, submit_y)
+        pyautogui.click(submit_x / 2, submit_y / 2, 2) # TODO: not clicking!
+        print("clicked!")
+        break
 
 for i in range(int(record_seconds * fps)):
     # make a screenshot
@@ -48,19 +55,19 @@ for i in range(int(record_seconds * fps)):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # https://docs.opencv.org/4.x/d4/dc6/tutorial_py_template_matching.html
-    submit_button = cv2.imread("./submit.png")
-    submit = cv2.matchTemplate(frame, submit_button, cv2.TM_CCOEFF)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(submit)
-    left, top = min_loc
-    bottom, right = max_loc
-    submit_center = pyautogui.center(Box(left, top, right - left, bottom - top))
-    submit_x, submit_y = submit_center
-    print(submit_x, submit_y)
-    pyautogui.click(submit_x, submit_y)
+    # submit_button = cv2.imread("./submit.png")
+    # submit = cv2.matchTemplate(frame, submit_button, cv2.TM_CCOEFF)
+    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(submit)
+    # left, top = min_loc
+    # bottom, right = max_loc
+    # submit_center = pyautogui.center(Box(left, top, right - left, bottom - top))
+    # submit_x, submit_y = submit_center
+    # print(submit_x, submit_y)
+    # pyautogui.click(submit_x, submit_y)
 
-    print(min_val, max_val, min_loc, max_loc)
-    if submit:
-        break
+    # print(min_val, max_val, min_loc, max_loc)
+    # if submit:
+    #     break
 
     # write the frame
     out.write(frame)
